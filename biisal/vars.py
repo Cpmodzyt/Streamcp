30ae70ead937f76a9f15739438ea364f497cccc7
Itz Cp <121564783+Cpmodzyt@users.noreply.github.com>
2024-06-12 06:20:09 +0530

    Add files via upload

--- biisal/vars.py
+++ biisal/vars.py
@@ -6,27 +6,27 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-bot_name = "Fɪʟᴇ2Lɪɴᴋ Bᴏᴛ"
-bisal_channel = "https://telegram.me/FilmZone_Official"
-bisal_grp = "https://t.me/FilmZone_talk"
+bot_name = "Bɪɪsᴀʟ Fɪʟᴇ2Lɪɴᴋ Bᴏᴛ"
+bisal_channel = "https://telegram.me/bisal_files"
+bisal_grp = "https://t.me/+PA8OPL2Zglk3MDM1"
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '25812247'))
-    API_HASH = str(getenv('API_HASH', '00a50dc03d5b63a0eabfca83fa02c4ca'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN' , '6738562342:AAHe16lKz07NYFH-nrXU5S2QXfW6SpHQ_8w'))
+    API_ID = int(getenv('API_ID', ''))
+    API_HASH = str(getenv('API_HASH', ''))
+    BOT_TOKEN = str(getenv('BOT_TOKEN' , ''))
     name = str(getenv('name', 'bisal_file2link_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002051680690'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002051680690'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', ''))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "6880594191").split()]
+    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "5397984467").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Itzmecp'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'biisal'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
@@ -39,8 +39,8 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://itzcp:itzcp@cluster0.rrphufd.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'FilmZone_talk')) 
+    DATABASE_URL = str(getenv('DATABASE_URL', ''))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'bisal_files')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
     BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))   
     BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.Pʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ @biisal_bot ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
